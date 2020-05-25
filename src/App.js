fa91c2178f48ca807e3901e35ff069613cb4026e
Zach Foltz <zfoltzy7@gmail.com>
2020-05-25 17:01:45 -0500

    added bootstrap and fontawesome support

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 import {BrowserRouter, Link, Route, Switch} from "react-router-dom";
 import Home from "./components/home";
 import ErrorBoundary from "./pages/errors/error-boundary";
+import {library} from "@fortawesome/fontawesome-svg-core"
+import {fab} from "@fortawesome/free-brands-svg-icons"
+import {faCheckSquare, faCoffee} from "@fortawesome/free-solid-svg-icons"
+
+library.add(fab, faCheckSquare, faCoffee);
 
 function App() {
     return (
--- src/components/home.js
+++ src/components/home.js
@@ -1,10 +1,16 @@
 import React from "react";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+
 
 function Home() {
 
     return (
         <div>
-            <p>Welcome Home!</p>
+            <h1>Welcome Home!</h1>
+            <a className="btn btn-link" href={"http://twistedbean.com/"} rel={"noopener noreferrer"} target={"_blank"}>
+                <FontAwesomeIcon icon="coffee" spin="true" className="mr-sm-1"/>
+                Coffee
+            </a>
         </div>
     )
 }
