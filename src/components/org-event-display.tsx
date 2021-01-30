632da82e58c51af05027b189595efeac57f91200
Zach Foltz <zfoltzy7@gmail.com>
2021-01-30 17:40:16 -0600

    formatting

--- src/components/org-event-display.tsx
+++ src/components/org-event-display.tsx
@@ -11,8 +11,12 @@ export default function OrgEventDisplay(props: Props) {
     <div>
       <h4>{orgEvent.name}</h4>
       {orgEvent.startDate && <p>Event date: {orgEvent.startDate}</p>}
-        <p><strong>Location:</strong> {orgEvent.location}</p>
-      <p><strong>Description:</strong> {orgEvent.description}</p>
+      <p>
+        <strong>Location:</strong> {orgEvent.location}
+      </p>
+      <p>
+        <strong>Description:</strong> {orgEvent.description}
+      </p>
     </div>
   );
 }
